aff246a51cb7691a803d19dcd4eab7d93a5d787a
MaiiconnDouglasd <maiicondouglasd@hotmail.com>
2021-11-11 12:45:29 -0300

    Login funcionar

--- Vue/pages/login.vue
+++ Vue/pages/login.vue
@@ -1,117 +1,130 @@
 <template>
   <main>
-  <!-- <Menu />  -->
+    <!-- <Menu />  -->
 
-     <br>  <br>  <br><br>  <br>  <br><br>  <br>  <br><br>  <br>  <br><br>  <br>  <br>
-  <body>
-    <div class="container">
-      <div class="forms-container">
-        <div class="signin-signup">
-          <form action="#" class="sign-in-form">
-            <h2 class="title">Sign in</h2>
-            <div class="input-field">
-              <i class="fas fa-user"></i>
-              <input type="text" placeholder="Username" />
+    <br />
+    <br />
+    <br /><br />
+    <br />
+    <br /><br />
+    <br />
+    <br /><br />
+    <br />
+    <br /><br />
+    <br />
+    <br />
+    <body>
+      <div class="container">
+        <div class="forms-container">
+          <div class="signin-signup">
+            <form action="#" class="sign-in-form"><br><br><br>
+              <h2 class="title">Sign in</h2>
+              <div class="input-field">
+                <i class="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div class="input-field">
+                <i class="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" value="Login" class="btn solid" />
+
+              <p class="social-text">Ou faça login com plataformas sociais</p>
+              <div class="social-media">
+                <a href="#" class="social-icon">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-google"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form>
+<!-- Registro -->
+            <!-- <form action="#" class="sign-up-form">
+              <h2 class="title">Inscrever-se</h2>
+
+              <div class="input-field">
+                <i class="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div class="input-field">
+                <i class="fas fa-envelope"></i>
+                <input type="email" placeholder="Email" />
+              </div>
+              <div class="input-field">
+                <i class="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" class="btn" value="Sign up" />
+              <p class="social-text">Ou inscreva-se em plataformas sociais</p>
+              <div class="social-media">
+                <a href="#" class="social-icon">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-google"></i>
+                </a>
+                <a href="#" class="social-icon">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form> -->
+          </div>
+        </div>
+
+        <div class="panels-container">
+          <div class="panel left-panel">
+            <div class="content">
+              <h3>Novo aqui ?</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Debitis, ex ratione. Aliquid!
+              </p>
+              <button class="btn transparent" id="sign-up-btn">
+                Inscrever-se
+              </button>
             </div>
-            <div class="input-field">
-              <i class="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
+            <img src="~/static/img/log.svg" class="image" alt="Entrar" />
+          </div>
+          <!-- Registro -->
+          <!-- <div class="panel right-panel">
+            <div class="content">
+              <h3>Um de nós ?</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                laboriosam ad deleniti.
+              </p>
+              <button class="btn transparent" id="sign-in-btn">Entrar</button>
             </div>
-            <input type="submit" value="Login" class="btn solid" />
-            
-            <p class="social-text">Ou faça login com plataformas sociais</p>
-            <div class="social-media">
-              <a href="#" class="social-icon">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-twitter"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-google"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-linkedin-in"></i>
-              </a>
-            </div>
-          </form>
-          <form action="#" class="sign-up-form">
-            <h2 class="title">Inscrever-se</h2>
-            <div class="input-field">
-              <i class="fas fa-user"></i>
-              <input type="text" placeholder="Username" />
-            </div>
-            <div class="input-field">
-              <i class="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" />
-            </div>
-            <div class="input-field">
-              <i class="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
-            <input type="submit" class="btn" value="Sign up" />
-            <p class="social-text">Ou inscreva-se em plataformas sociais</p>
-            <div class="social-media">
-              <a href="#" class="social-icon">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-twitter"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-google"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-linkedin-in"></i>
-              </a>
-            </div>
-          </form>
+            <img
+              src="~/static/img/register.svg"
+              class="image"
+              alt="Registrar"
+            />
+          </div> -->
         </div>
       </div>
 
-      <div class="panels-container">
-        <div class="panel left-panel">
-          <div class="content">
-            <h3>Novo aqui ?</h3>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-              ex ratione. Aliquid!
-            </p>
-            <button class="btn transparent" id="sign-up-btn">
-             Inscrever-se
-            </button>
-          </div>
-           <img src="src/imagem/log.svg" class="image" alt="Entrar" />
-        </div>
-        <div class="panel right-panel">
-          <div class="content">
-            <h3>Um de nós ?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              laboriosam ad deleniti.
-            </p>
-            <button class="btn transparent" id="sign-in-btn">
-              Entrar
-            </button>
-          </div>
-          <img src="src/imagem/register.svg" class="image" alt="Registrar" />
-        </div>
-      </div>
-    </div>
-    <script src="src/js/app.js"></script>
-  </body>
-   <script
-      src="https://kit.fontawesome.com/64d58efce2.js"  crossorigin="anonymous"    ></script>
+      <script src="~/static/js/app.js"></script>
+    </body>
 
-     <br>  <br>  <br><br>  <br>  <br><br>  <br>  <br><br>  <br>  <br><br>  <br>  <br>
-        <br>  <br>  <br><br>  <br>  <br><br>  <br><br>  
-
-   <Footer />
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"
+    ></script>
   </main>
 </template>
 
 <style scoped>
-
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap");
 
 * {
@@ -512,5 +525,4 @@ form.sign-in-form {
     left: 50%;
   }
 }
-
 </style>
